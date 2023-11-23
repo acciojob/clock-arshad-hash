9aa26e567274f1fe883d80deb9592ce4d8400fd8
arshad-hash <142026560+arshad-hash@users.noreply.github.com>
2023-11-23 18:04:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 //your JS code here. If required.
+function updateTimer() {
+  let now = new Date();
+
+  let options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric', hour: '2-digit', minute: '2-digit', second: '2-digit' };
+  let formattedDate = now.toLocaleDateString('en-US', options);
+
+  document.getElementById('timer').textContent = formattedDate;
+}
+
+// Call the function to display the initial date and time
+updateTimer();
+
+// Update the timer every second
+setInterval(updateTimer, 1000);
